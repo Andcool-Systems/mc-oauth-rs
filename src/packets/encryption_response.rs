34f7a693cccca3513486cb5f4ca7b0c46425fcd7
Andcool-Systems <drolll2007@gmail.com>
2025-05-25 18:14:49 +0300

    refactor: Removed dead code

--- src/packets/encryption_response.rs
+++ src/packets/encryption_response.rs
@@ -1,14 +1,9 @@
+use crate::byte_buf_utils::read_varint;
 use anyhow::Result;
 use bytes::BytesMut;
 
-use crate::byte_buf_utils::read_varint;
-
-#[allow(dead_code)]
 pub struct EncryptionResponsePacket {
-    pub shared_secret_length: usize,
     pub shared_secret: Vec<u8>,
-
-    pub verify_token_length: usize,
     pub verify_token: Vec<u8>,
 }
 
@@ -21,9 +16,7 @@ impl EncryptionResponsePacket {
         let verify_token = buff.split_to(verify_token_length);
 
         Ok(Self {
-            shared_secret_length,
             shared_secret: shared_secret.to_vec(),
-            verify_token_length,
             verify_token: verify_token.to_vec(),
         })
     }
